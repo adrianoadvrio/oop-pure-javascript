5fdba947323c270d112b1b8bb6fdb24024ccbd00
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-22 13:59:25 -0300

    lunch time...

---
+++ js/script1.js
@@ -0,0 +1,23 @@
+class Produto {
+
+    constructor(){
+    
+    this.id = 0;
+    this.nomeProduto = "";
+    this.valor = 0;
+
+    }
+
+    adicionar(){
+
+        alert("vamos adicionar um produto");
+    }
+
+    excluir(){
+      
+        alert("Item deletado!");
+
+    }
+}
+
+var produto = new Produto();
