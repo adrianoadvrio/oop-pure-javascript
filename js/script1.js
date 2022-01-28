78423190cd4e626b0842cac6e875ab517dcc921f
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-28 07:45:57 -0300

    working put elements

--- js/script1.js
+++ js/script1.js
@@ -14,10 +14,36 @@ class Produto {
             this.adicionar(produto);
            
         }
-       console.log(this.arrayProdutos);
+       
+        this.listaTabela();
     }
 
-    adicionar(){
+
+     listaTabela(){
+
+    let tbody = document.getElementById('tbody')
+    tbody.innerText = '';
+
+    for(let i=0; i<this.arrayProdutos.length; i++){
+
+        let tr = tbody.insertRow();
+
+        let td_id = tr.insertCell();
+        let td_produto = tr.insertCell();
+        let td_valor = tr.insertCell();
+        let td_acoes = tr.insertCell();
+
+        td_id.innerText = this.arrayProdutos[i].id;
+        td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+        td_valor.innerText = this.arrayProdutos[i].preco;
+
+        }
+
+
+     }
+
+
+    adicionar(produto){
       this.arrayProdutos.push(produto);
       this.id++;
 
