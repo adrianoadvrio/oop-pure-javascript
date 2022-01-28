93e2235ac60d8b700bdb4d4267499128d5419928
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-28 07:08:52 -0300

    working on arrays

--- js/script1.js
+++ js/script1.js
@@ -2,20 +2,63 @@ class Produto {
 
     constructor(){
     
-    this.id = 0;
-    this.nomeProduto = "";
-    this.valor = 0;
-
+    this.id = 1;
+    this.arrayProdutos = [];
+    
     }
 
     salvar(){
-
-        alert("vamos adicionar um produto");
+        let produto = this.lerDados();
+        
+        if(this.validaCampos(produto)){
+            this.adicionar(produto);
+           
+        }
+       console.log(this.arrayProdutos);
     }
 
-    excluir(){
+    adicionar(){
+      this.arrayProdutos.push(produto);
+      this.id++;
+
+    }
+ 
+    lerDados(){
+        let produto = {};
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = document.getElementById('preco').value;
+
+        return produto;
+
+    }
+
+    validaCampos(produto){
+        let msg = '';
+
+        if(produto.nomeProduto == ''){
+            msg+= 'Informe o nome do produto.\n '
+        }
+
+
+        if(produto.preco == ''){
+            msg+= 'Informe o preço do produto.\n'
+        }
+
+
+        if(msg != ''){
+            alert(msg);
+            return false;
+        }
+
+        return true;
+
+    }
+
+    cancelar(){
       
-        alert("Item deletado!");
+        
 
     }
 }
